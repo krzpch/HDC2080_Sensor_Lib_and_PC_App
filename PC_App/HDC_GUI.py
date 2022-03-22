a6744f62ed43d4a707883d19e789218738d63630
Krzysztof Półchłopek <polchloek.krzysztof@gmail.com>
2022-03-22 19:18:39 +0100

    Added base for GUI

---
+++ PC_App/HDC_GUI.py
@@ -0,0 +1,16 @@
+##
+# @author: Krzysztof Półchłopek
+# @date: 22.03.2022
+#
+
+import sys
+import os
+import serial
+from PyQt5 import QtWidgets, uic
+
+class MainGUI(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(MainGUI, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('mainWindow.ui', self) # Load the .ui file
+        self.show() # Show the GUI
--- PC_App/HDC_Main.py
+++ PC_App/HDC_Main.py
@@ -1,8 +1,13 @@
+##
+# @author: Krzysztof Półchłopek
+# @date: 22.03.2022
+#
+
 from PyQt5.QtWidgets import QApplication
 import sys
 from HDC_GUI import MainGUI
 
-
+## Main program function
 def main():
     app = QApplication(sys.argv)
     window = MainGUI()
