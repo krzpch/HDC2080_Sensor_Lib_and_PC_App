d8f307549184462953b6c4bdb0c2ca342e168edb
Krzysztof Półchłopek <polchloek.krzysztof@gmail.com>
2022-03-22 17:38:54 +0100

    Main file

---
+++ PC_App/HDC_Main.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+from HDC_GUI import MainGUI
+def main():
+    app = QApplication(sys.argv)
+    window = MainGUI()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
