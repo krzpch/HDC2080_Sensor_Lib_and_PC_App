2b62975bb13fc5c8590b6de154af20aebeca2d20
Krzysztof Półchłopek <polchloek.krzysztof@gmail.com>
2022-03-31 11:33:29 +0200

    Added port select

--- PC_App/HDC_GUI.py
+++ PC_App/HDC_GUI.py
@@ -4,6 +4,9 @@
 #
 
 from PyQt5 import QtWidgets, QtCore, uic
+import serial
+import sys
+import glob
 
 from HDC_UartThread import UARTThread
 
@@ -14,3 +17,47 @@ class MainGUI(QtWidgets.QMainWindow):
         super(MainGUI, self).__init__()
         uic.loadUi('mainWindow.ui', self) # Load the .ui file
         self.show() # Show the GUI
+
+        # update prot select box 
+        for port in self.list_ports():
+            self.portSelectBox.addItem(port, port)
+
+        #attach functions to buttons
+        self.portRefreshButton.clicked.connect(self.refresh_portSelectBox_onClick)
+
+
+    ## Funcion to update temperature display
+    def updateTempLCD(self, temp):
+        self.temp_lcdNumber.display(temp)
+
+
+    ## Funcion to update humidity display
+    def updateHumLCD(self, hum):
+        self.hum_lcdNumber.display(hum)
+
+
+    ## Funtion for listing all avaliable ports 
+    def list_ports(self):
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            ports = glob.glob('/dev/ttyS[0-90-9]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
+    
+    ## Function for updating portSelectBox
+    def refresh_portSelectBox_onClick(self):
+        self.portSelectBox.clear()
+        for port in self.list_ports():
+            self.portSelectBox.addItem(port, port)
